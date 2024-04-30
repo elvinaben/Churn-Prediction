87990bae3e3fe1a192870886e5bd1e89daa10b90
elvinaben <119756091+elvinaben@users.noreply.github.com>
2024-04-30 13:45:03 +0700

    Add files via upload

---
+++ prediction1.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load('Xgboost_2.pkl')
+
+def main():
+    st.title('Prediction')
+
+    Unnamed: 0 = 0
+
+    id = st.number_input("ID", 1, 999999) # min:3, max:165033
+
+    CustomerId = st.number_input("Customer ID", 10000000, 9999999999) # cek lg! min:1.556570e+07, max:1.581569e+07
+
+    CreditScore = st.number_input("Credit Score", 100, 1000)
+
+    Geography_Other = 0
+    Geography_Germany = 0
+    Geography_France = 0
+    Geography_Spain = 0
+
+    Geography_selection = st.radio("Country", ('Germany', 'France', 'Spain'))
+    if Geography_selection == 'Germany':
+        Geography_Germany = 1  
+    elif Geography_selection == 'France':
+        Geography_France = 1
+    else:
+        Geography_Spain = 1 
+
+    Gender_selection = st.radio("Gender?", ('Male', 'Female'))
+    if Gender_selection == 'Male':
+        Gender = 1
+    else:
+        Gender = 0
+
+    Age = st.number_input("Age", 17, 100)
+
+    Tenure = st.slider("Tenure", min_value=0, max_value=10, value=1)
+
+    Balance = st.number_input("Balance", 0, 1000000)
+
+    NumOfProducts = st.slider("Number Of Products", min_value=1, max_value=4, value=1)
+    
+    CC_selection = st.radio("Have Credit Card?", ('Yes', 'No'))
+    if CC_selection == 'Yes':
+        HasCrCard = 1
+    else:
+        HasCrCard = 0
+
+    ActiveMember_selection = st.radio("Active Member?", ('Yes', 'No'))
+    if ActiveMember_selection == 'Yes':
+        IsActiveMember = 1
+    else:
+        IsActiveMember = 0 
+
+    EstimatedSalary = st.number_input("Estimated Salary", 0, 1000000)
+    
+    if st.button('Make Prediction'):
+        features = [CreditScore, Gender, Age, Tenure, Balance, NumOfProducts, 
+                    HasCrCard, IsActiveMember, EstimatedSalary, Geography_France, 
+                    Geography_Germany, Geography_Spain, Geography_Other]
+        result = make_prediction(features)
+        st.success(f'Result: {result}')
+
+def make_prediction(features):
+    input_array = np.array(features).reshape(1, -1)
+    prediction = model.predict(input_array)
+    return prediction[0]
+
+if __name__ == '__main__':
+    main()
+
+# run di lokal
+# streamlit run prediction.py
